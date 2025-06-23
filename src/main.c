f8fb7a02f7ca298c27af4b2d634067d4f928a6f5
IllusiveBagel <logan@neutral.systems>
2025-06-23 21:38:41 +0100

    Implemented Framebuffer with simple example

--- src/main.c
+++ src/main.c
@@ -1,22 +1,18 @@
 #include "uart.h"
-#include "power.h"
+#include "lfb.h"
 
 void main()
 {
-    char c;
-
-    // set up serial console
+    // set up serial console and linear frame buffer
     uart_init();
+    lfb_init();
 
+    // display a pixmap
+    lfb_showpicture();
+
+    // echo everything back
     while (1)
     {
-        uart_puts(" 1 - power off\n 2 - reset\nChoose one: ");
-        c = uart_getc();
-        uart_send(c);
-        uart_puts("\n\n");
-        if (c == '1')
-            power_off();
-        if (c == '2')
-            reset();
+        uart_send(uart_getc());
     }
 }
